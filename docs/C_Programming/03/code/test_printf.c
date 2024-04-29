2a4193ad9f664c883e850dde8cac76342fb439f2
caiyouy <yuancaiyou@hotmail.com>
2024-04-29 16:11:24 +0800

    change theme to PaperMod

--- docs/C_Programming/03/code/test_printf.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-        int i=43;
-        printf("%d",printf("%d",printf("%d",i)));
-        printf("\n");
-        return 0;
-}
--- docs/C_Programming/09/code/sort/sort.h
+++
@@ -1,6 +0,0 @@
-void select_sort(int a[], int n);
-void bubble_sort(int a[],int n);
-void insert_sort(int a[],int n);
-void shell_sort(int a[],int n);
-void quick_sort(int a[],int n);
-void print(int a[], int n);
